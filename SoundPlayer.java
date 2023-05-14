5117c2899857bf9762d7a08edba3e5aeef542709
Morris <morrism6@vdi-scclab-067.lancs.local>
2023-05-14 20:35:20 +0100

    Added SoundPlayer class which plays sounds

---
+++ SoundPlayer.java
@@ -0,0 +1,29 @@
+import  java.io.*; 
+import java.net.URL; 
+import javax.sound.sampled.*;
+import javax.swing.*;
+
+
+public class SoundPlayer {
+    public SoundPlayer() {
+        
+    };
+
+    public void PlaySound(String soundName) {
+        try {
+            URL url = this.getClass().getClassLoader().getResource(soundName);
+            AudioInputStream audioIn = AudioSystem.getAudioInputStream(url);
+            Clip clip = AudioSystem.getClip(); 
+            clip.open(audioIn);
+            clip.start();
+        } catch (UnsupportedAudioFileException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (LineUnavailableException e) {
+            e.printStackTrace();
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace();        
+        }
+    }
+}
